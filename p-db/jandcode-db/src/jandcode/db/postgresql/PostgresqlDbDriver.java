33323a26c4a040451e9c2a462e6944a7f0398f3e
Sergey Kravchenko <gkraser@gmail.com>
2019-05-02 16:15:52 +0600

    ~~
    --HG--
    branch : dev

---
+++ p-db/jandcode-db/src/jandcode/db/postgresql/PostgresqlDbDriver.java
@@ -0,0 +1,32 @@
+package jandcode.db.postgresql;
+
+import jandcode.commons.variant.*;
+import jandcode.db.*;
+import jandcode.db.base.*;
+
+import java.sql.*;
+import java.util.*;
+
+public class PostgresqlDbDriver extends BaseDbDriver {
+
+    public void assignStatementParams(PreparedStatement statement, IVariantNamed paramValues, List<String> paramNames) throws Exception {
+        // postgre не конвертирует автоматом string->int, поэтому тут берем типы параметров за основу
+        ParameterMetaData md = statement.getParameterMetaData();
+        for (int i = 0; i < paramNames.size(); i++) {
+            String paramName = paramNames.get(i);
+            int paramIdx = i + 1;
+
+            VariantDataType dt = paramValues.getDataType(paramName);
+
+            if (paramValues.isNull(paramName)) {
+                setNullParam(statement, paramIdx, dt);
+            } else {
+                Object value = paramValues.getValue(paramName);
+                int ptyp = md.getParameterType(paramIdx);
+                DbDataType dbdt = getDbDataType(ptyp);
+                dbdt.setValue(statement, paramIdx, value);
+            }
+        }
+    }
+
+}
