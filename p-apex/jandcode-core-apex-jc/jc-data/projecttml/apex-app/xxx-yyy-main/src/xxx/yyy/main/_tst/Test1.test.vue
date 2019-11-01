1347386619005865ba632e0c3b6f3e828c069dca
Sergey Kravchenko <gkraser@gmail.com>
2019-11-01 08:00:10 +0600

    ~~
    --HG--
    branch : dev

---
+++ p-apex/jandcode-core-apex-jc/jc-data/projecttml/apex-app/xxx-yyy-main/src/xxx/yyy/main/_tst/Test1.test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Test vue
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {},
+        data() {
+            return {}
+        }
+    }
+</script>
