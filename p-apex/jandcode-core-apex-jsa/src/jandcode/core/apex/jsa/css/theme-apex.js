69c7de3faadd9f5ee167c30b85d2ba64f7dcf762
Sergey Kravchenko <gkraser@gmail.com>
2019-11-06 09:28:12 +0600

    theme sass
    --HG--
    branch : dev

--- p-apex/jandcode-core-apex-jsa/src/jandcode/core/apex/jsa/css/theme-apex.js
+++ p-apex/jandcode-core-apex-jsa/src/jandcode/core/apex/jsa/css/theme-apex.js
@@ -1,6 +1,6 @@
-import cssQuasar from './quasar-style.scss'
-import css from './theme-apex.less'
+/* Тема apex
+----------------------------------------------------------------------------- */
+import css from './theme-apex.scss'
 import '[*]/css/module-apex.js'
 
-Jc.requireCss(cssQuasar)
 Jc.requireCss(css)
