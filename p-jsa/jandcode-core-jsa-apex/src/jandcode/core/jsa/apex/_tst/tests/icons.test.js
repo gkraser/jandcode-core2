a2de3e29391273a46695ca860c405102f02085d6
Sergey Kravchenko <gkraser@gmail.com>
2019-11-22 11:44:38 +0600

    ~~
    --HG--
    branch : dev

--- p-jsa/jandcode-core-jsa-apex/src/jandcode/core/jsa/apex/_tst/tests/icons.test.js
+++ p-jsa/jandcode-core-jsa-apex/src/jandcode/core/jsa/apex/_tst/tests/icons.test.js
@@ -2,7 +2,7 @@ import {apex, test} from './vendor'
 
 import * as m from '../../js/utils/icons'
 
-describe(__filename, function() {
+describe('icons.test.js', function() {
 
     it("1", function() {
         console.info(m.getIcons());
