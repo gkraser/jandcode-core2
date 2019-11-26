49403dec9fcc75ea2a7ed9e9ed1d14a16d95461d
Sergey Kravchenko <gkraser@gmail.com>
2019-11-26 07:07:57 +0600

    jc: runtimeCtx убран
    --HG--
    branch : dev

--- p-jc/jandcode-jc/src/jandcode/jc/CtxFactory.java
+++ p-jc/jandcode-jc/src/jandcode/jc/CtxFactory.java
@@ -1,40 +1,14 @@
 package jandcode.jc;
 
-import jandcode.commons.error.*;
 import jandcode.jc.impl.*;
 
 public class CtxFactory {
-
-    private static Ctx runtimeCtx;
 
     /**
      * Создание нового экземпляра контекста
      */
     public static Ctx createCtx() {
         return new CtxImpl();
-    }
-
-    /**
-     * Глобальный ctx, который можно использовать в runtime
-     */
-    public static Ctx getRuntimeCtx() {
-        if (runtimeCtx == null) {
-            synchronized (CtxFactory.class) {
-                if (runtimeCtx == null) {
-                    Ctx ctx = new CtxImpl(true);
-
-                    try {
-                        JcConfig cfg = JcConfigFactory.create();
-                        ctx.applyConfig(cfg);
-                    } catch (Exception e) {
-                        throw new XErrorWrap(e);
-                    }
-
-                    runtimeCtx = ctx;
-                }
-            }
-        }
-        return runtimeCtx;
     }
 
 }
