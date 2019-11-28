f7ad7597aef9fc7d8d88e21125f939f032e6f76b
Sergey Kravchenko <gkraser@gmail.com>
2019-11-28 06:00:25 +0600

    ~~
    --HG--
    branch : dev

--- p-jsa/jandcode-core-jsa-apex/src/jandcode/core/jsa/apex/css/std-theme.js
+++ p-jsa/jandcode-core-jsa-apex/src/jandcode/core/jsa/apex/css/std-theme.js
@@ -1,12 +1,11 @@
 /* Тема std
 ----------------------------------------------------------------------------- */
 import cssQuasar from './std/z-quasar-style.scss'
-import cssRobotoFont from '@quasar/extras/roboto-font/roboto-font.css'
 import css from './std-theme.less'
 
 let config = require('[*]/css/std-config.js')
 
 export default {
-    css: [cssRobotoFont, cssQuasar, css],
+    css: [cssQuasar, css],
     config: config
 }
