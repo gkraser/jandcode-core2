aa82de67f38e63f2ce1d3dacdfff4292d0d24999
Sergey Kravchenko <gkraser@gmail.com>
2019-12-03 08:10:46 +0600

    ~~
    --HG--
    branch : dev

--- p-web/jandcode-core-web/src/jandcode/core/web/action/ActionMethodWrapper.java
+++ p-web/jandcode-core-web/src/jandcode/core/web/action/ActionMethodWrapper.java
@@ -3,10 +3,17 @@ package jandcode.core.web.action;
 import java.lang.reflect.*;
 
 /**
- *
+ * Если action имеет один параметр с наследником этого типа,
+ * то создается этот экземпляр и ему делегируется выполнения метода action.
  */
 public interface ActionMethodWrapper {
 
+    /**
+     * Выполнить метод action
+     *
+     * @param inst   экземпляр action (обычно {@link BaseAction})
+     * @param method метод, который нужно выполнить в inst
+     */
     void execActionMethod(Object inst, Method method) throws Exception;
 
 }
