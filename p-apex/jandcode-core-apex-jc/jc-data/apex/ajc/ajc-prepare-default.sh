cd53ad3e32a0e54ceb13d60fe1f76a117be56fcc
Sergey Kravchenko <gkraser@gmail.com>
2019-12-04 20:19:15 +0600

    linux
    --HG--
    branch : dev

--- p-apex/jandcode-core-apex-jc/jc-data/apex/ajc/ajc-prepare-default.sh
+++ p-apex/jandcode-core-apex-jc/jc-data/apex/ajc/ajc-prepare-default.sh
@@ -2,8 +2,8 @@
 
 CP=?
 JVM=
-JVM=${JVM} -cp ${CP}
-JVM=${JVM} -Djandcode.app.appdir=${WD}
-JVM=${JVM} -Djandcode.consolecharset=auto
-JVM=${JVM} -Dfile.encoding=UTF-8
+JVM="${JVM} -cp ${CP}"
+JVM="${JVM} -Djandcode.app.appdir=${WD}"
+JVM="${JVM} -Djandcode.consolecharset=auto"
+JVM="${JVM} -Dfile.encoding=UTF-8"
 MAIN=?
--- p-apex/jandcode-core-apex-jc/jc-data/apex/ajc/ajc-product-default.sh
+++ p-apex/jandcode-core-apex-jc/jc-data/apex/ajc/ajc-product-default.sh
@@ -5,10 +5,10 @@ WD=`dirname $0`
 CP=${WD}/lib:${WD}/lib/*
 
 JVM=
-JVM=${JVM} -cp %CP%
-JVM=${JVM} -Djandcode.app.appdir=${WD}
-JVM=${JVM} -Djandcode.consolecharset=auto
-JVM=${JVM} -Dfile.encoding=UTF-8
+JVM="${JVM} -cp ${CP}"
+JVM="${JVM} -Djandcode.app.appdir=${WD}"
+JVM="${JVM} -Djandcode.consolecharset=auto"
+JVM="${JVM} -Dfile.encoding=UTF-8"
 MAIN=?
 
 java ${JVM} ${MAIN} $*
