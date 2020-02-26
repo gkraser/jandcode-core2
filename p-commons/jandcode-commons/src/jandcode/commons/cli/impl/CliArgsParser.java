0e2bad400732f55adfcaa8d23b88480427206237
Sergey Kravchenko <gkraser@gmail.com>
2020-02-26 09:58:44 +0600

    commons: cli
    --HG--
    branch : dev

--- p-commons/jandcode-commons/src/jandcode/commons/cli/impl/CliArgsParser.java
+++ p-commons/jandcode-commons/src/jandcode/commons/cli/impl/CliArgsParser.java
@@ -9,8 +9,6 @@ import jandcode.commons.cli.*;
  */
 public class CliArgsParser {
 
-    public static final String DEFAULT_OPT_VALUE = "1";
-
     public CliArgs parse(String[] args) {
         CliArgsImpl res = new CliArgsImpl(args);
         if (args == null || args.length == 0) {
@@ -24,13 +22,13 @@ public class CliArgsParser {
             if (arg.startsWith("-")) {
                 // option
                 String opt = arg.substring(1);
-                String value = DEFAULT_OPT_VALUE;
+                String value = CliArgs.DEFAULT_OPT_VALUE;
                 int a = opt.indexOf(':');
                 if (a != -1) {
                     value = opt.substring(a + 1);
                     opt = opt.substring(0, a);
                     if (UtString.empty(value)) {
-                        value = DEFAULT_OPT_VALUE;
+                        value = CliArgs.DEFAULT_OPT_VALUE;
                     }
                 }
                 res.put(opt, value);
