efe2438168f2e2e4d41be3b67a4b75219ad13cc3
Sergey Kravchenko <gkraser@gmail.com>
2020-03-06 08:35:16 +0600

    ~~
    --HG--
    branch : dev

--- p-apex/jandcode-core-apex-jc/jc-data/projecttml/apex-app/xxx-yyy-main/src/xxx/yyy/main/AppLauncher.java
+++ p-apex/jandcode-core-apex-jc/jc-data/projecttml/apex-app/xxx-yyy-main/src/xxx/yyy/main/AppLauncher.java
@@ -2,10 +2,10 @@ package xxx.yyy.main;
 
 import jandcode.core.apex.launcher.*;
 
-public class AppLauncher {
+public class AppLauncher extends ApexLauncher {
 
     public static void main(String[] args) throws Exception {
-        new ApexLauncher().run(args);
+        new AppLauncher().run(args);
     }
 
 }
