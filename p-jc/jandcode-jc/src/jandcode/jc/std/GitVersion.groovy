da99434a1f06bf179be2d89579954a2904a2d0b9
gkraser <gkraser@gmail.com>
2020-07-22 11:21:51 +0600

    git

---
+++ p-jc/jandcode-jc/src/jandcode/jc/std/GitVersion.groovy
@@ -0,0 +1,133 @@
+package jandcode.jc.std
+
+import jandcode.commons.*
+import jandcode.jc.*
+import jandcode.jc.impl.version.*
+
+/**
+ * Работа с версией, получаемой из git
+ */
+class GitVersion extends ProjectScript {
+
+    Ver verCached
+
+    class Ver extends BaseVersion {
+
+        String cache
+
+        Ver() {
+        }
+
+        String getText() {
+            if (cache == null) {
+                cache = getVer()
+            }
+            return cache
+        }
+
+    }
+
+    String workDir
+
+    /**
+     * Рабочий каталог, относительно которого будет работать git
+     */
+    String getWorkDir() {
+        if (this.workDir == null) {
+            return project.wd("")
+        }
+        return workDir
+    }
+
+    /**
+     * Автогенерируемая версия (по тегам в git) для рабочего каталога проекта.
+     * Если текущий тег 'v-XXX', то возвращает 'XXX', иначе возвращает
+     * версию 'XXX-COMMITHASH' из последнего тега 'v-XXX'
+     */
+    public String getVer() {
+        def v = ""
+        try {
+            def f = UtFile.findFileUp(".git", getWorkDir())
+            if (f != null) {
+                def a = ut.runcmd(cmd: 'git describe --match "v-*"', saveout: true, showout: false, dir: getWorkDir())
+                v = ""
+                if (a.size() > 0) {
+                    if (a[0].startsWith("v-")) {
+                        v = a[0].substring(2)
+                    }
+                }
+            }
+        } catch (ignore) {
+        }
+        if (v == "") {
+            v = JcConsts.VERSION_DEFAULT + "-" + rev
+        }
+        return v
+    }
+
+    /**
+     * Номер ревизии исходников.
+     * hash текущего коммита.
+     */
+    public String getRev() {
+        try {
+            def f = UtFile.findFileUp(".git", getWorkDir())
+            if (f != null) {
+                def a = ut.runcmd(cmd: 'git log --pretty=format:%h -n 1', saveout: true, showout: false, dir: getWorkDir())
+                if (a.size() > 0) {
+                    def s = a[0]
+                    return s
+                }
+            }
+        } catch (ignore) {
+        }
+        return "DEV"
+    }
+
+    /**
+     * Автогенерируемая версия (по тегам в git).
+     * Если текущий тег 'v-XXX', то возвращает 'XXX', иначе возвращает
+     * версию 'XXX-COMMITHASH' из последнего тега 'v-XXX'
+     */
+    public IVersion getVersion() {
+        if (verCached == null) {
+            verCached = new Ver()
+        }
+        return verCached
+    }
+
+    /**
+     * Возвращает список версий. Первая в списке - самая последняя
+     */
+    public List getVersions() {
+        def v = []
+        try {
+            def f = UtFile.findFileUp(".git", getWorkDir())
+            if (f != null) {
+                def a = ut.runcmd(cmd: 'git tag --sort=-creatordate', saveout: true, showout: false, dir: getWorkDir())
+                if (a.size() > 0) {
+                    for (vs in a) {
+                        if (vs.startsWith("v-")) {
+                            v.add(vs.substring(2))
+                        }
+                    }
+                }
+            }
+        } catch (ignore) {
+        }
+        return v
+    }
+
+    /**
+     * Возвращает последнюю версию (по тегам git)
+     */
+    public String getLastVersion() {
+        def v = versions
+        if (v.size() == 0) {
+            return ""
+        } else {
+            return v[0]
+        }
+    }
+
+}
