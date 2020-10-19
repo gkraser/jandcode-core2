d92751e5cf690308df85390cd46d999a75752956
gkraser <gkraser@gmail.com>
2020-10-19 12:16:56 +0600

    ~~

--- p-apex/jandcode-apex-webapp/src/jandcode/apex/webapp/js/index.js
+++ p-apex/jandcode-apex-webapp/src/jandcode/apex/webapp/js/index.js
@@ -1,1 +1,12 @@
 //
+import './init'
+import {Vue, VueRouter, Quasar, jsaBase} from './vendor'
+
+//////
+
+export {
+    Vue,
+    VueRouter,
+    Quasar,
+    jsaBase,
+}
---
+++ p-apex/jandcode-apex-webapp/src/jandcode/apex/webapp/js/init.js
@@ -0,0 +1,10 @@
+/* Инициализация
+----------------------------------------------------------------------------- */
+
+//
+import {Quasar} from './vendor'
+
+// quasar lang
+let qLangRu = require('quasar/dist/lang/ru.umd.min.js')
+Quasar.lang.set(qLangRu)
+
