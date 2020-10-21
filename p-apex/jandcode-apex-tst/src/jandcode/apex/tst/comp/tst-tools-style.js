6e74e5866e3339fdcc4079a6dac1d5bf1a3b0fd4
gkraser <gkraser@gmail.com>
2020-10-21 09:00:18 +0600

    apex: tst

---
+++ p-apex/jandcode-apex-tst/src/jandcode/apex/tst/comp/tst-tools-style.js
@@ -0,0 +1,9 @@
+/* Подключение стилей .tst-tools
+----------------------------------------------------------------------------- */
+
+import q_css from './css/tst-tools-quasar-style.scss'
+import s_css from './css/tst-tools-style.less'
+
+Jc.requireCss(q_css, 'before-theme')
+Jc.requireCss(s_css, 'before-theme')
+
