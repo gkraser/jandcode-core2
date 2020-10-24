821170e0fd7bfc22206b62ff2efd4537451721dc
gkraser <gkraser@gmail.com>
2020-10-24 10:57:44 +0600

    ~~

---
+++ p-apex/jandcode-core-apex-webapp/src/jandcode/core/apex/webapp/_tst/pages/JcDecorAppStd1.test.vue
@@ -0,0 +1,89 @@
+<template>
+    <tst-apex-panel class="jcdecorappstd1-test-884f6234">
+
+        <template #tools>
+            <tst-checkbox v-model="cfg.debugBg" label="debugBg"/>
+            <tst-select v-model="cfg.toolbarSetLeft" :options="toolbarSets"
+                        label="toolbar left"/>
+            <tst-select v-model="cfg.toolbarSetRight" :options="toolbarSets"
+                        label="toolbar right"/>
+        </template>
+
+        <App container style="height: 500px; height: 70vh" :own="this">
+            <template #toolbar-left v-if="cfg.toolbarSetLeft">
+                <AppToolbarDemoSet :toolbarSet="cfg.toolbarSetLeft"/>
+            </template>
+
+            <template #toolbar-right>
+                <AppToolbarDemoSet :toolbarSet="cfg.toolbarSetRight"/>
+            </template>
+
+            <template #left>
+            </template>
+
+            <template #main>
+                <q-page padding>
+                    <jc-btn label="hello"/>
+                </q-page>
+            </template>
+        </App>
+
+    </tst-apex-panel>
+</template>
+
+<script>
+import {apex} from '../vendor'
+
+import AppToolbarDemoSet, {createToolbarSets, SubMenu1} from './_components/AppToolbarDemoSet'
+
+export default {
+    extends: Vue.component('tst-apex-page'),
+    mixins: [apex.JcApp],
+    components: {
+        App: apex.components.JcDecorAppStd,
+        AppToolbarDemoSet,
+        SubMenu1,
+    },
+    created() {
+        this.cfgStore.applyDefault({
+            debugBg: false,
+            toolbarSetRight: 'set1',
+            toolbarSetLeft: null,
+        })
+    },
+    data() {
+        return {
+            toolbarSets: createToolbarSets()
+        }
+    },
+    methods: {
+        applyCfg() {
+            let cfg = this.cfg
+
+            document.body.classList.toggle("debug-bg", cfg.debugBg)
+
+        },
+    }
+}
+</script>
+
+<style lang="less">
+
+// Отладочные стили
+.debug-bg {
+  .jc-toolbar {
+    background-color: #C6E2A6 !important;
+    color: black;
+
+    & > * {
+      background-color: #d7edc5;
+    }
+  }
+}
+
+.jcdecorappstd1-test-884f6234 {
+
+
+}
+
+</style>
