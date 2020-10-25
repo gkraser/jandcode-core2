ae76e3ccf3ba4b147f2e78baa7885e961fcfb6c7
gkraser <gkraser@gmail.com>
2020-10-25 10:05:24 +0600

    apex: roboto font

--- p-apex/jandcode-core-apex-webapp/src/jandcode/core/apex/webapp/css/apex-std-theme.js
+++ p-apex/jandcode-core-apex-webapp/src/jandcode/core/apex/webapp/css/apex-std-theme.js
@@ -3,9 +3,14 @@
 import cssQuasar from './apex-std/z-quasar-style.scss'
 import css from './apex-std-theme.less'
 
+import f400 from "fontsource-roboto/400.css";
+import f100 from "fontsource-roboto/100.css";
+import f400c from "fontsource-roboto-condensed/400.css";
+import f300c from "fontsource-roboto-condensed/300.css";
+
 let config = require('[*]/css/apex-std-config.js')
 
 export default {
-    css: [cssQuasar, css],
+    css: [f400, f100, f400c, f300c, cssQuasar, css],
     config: config
 }
