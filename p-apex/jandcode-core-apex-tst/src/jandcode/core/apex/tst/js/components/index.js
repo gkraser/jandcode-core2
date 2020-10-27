c13a0ffe6c02c62abda69135c5479d7c505dbf57
gkraser <gkraser@gmail.com>
2020-10-27 08:31:51 +0600

    apex

--- p-apex/jandcode-core-apex-tst/src/jandcode/core/apex/tst/js/components/index.js
+++ p-apex/jandcode-core-apex-tst/src/jandcode/core/apex/tst/js/components/index.js
@@ -11,12 +11,14 @@ import TstApexPage from './TstApexPage'
 import TstBtn from './TstBtn'
 import TstCheckbox from './TstCheckbox'
 import TstSelect from './TstSelect'
+import TstFontsize from './TstFontsize'
 
 Vue.component(TstApexPanel.name, TstApexPanel)
 Vue.component(TstApexPage.name, TstApexPage)
 Vue.component(TstBtn.name, TstBtn)
 Vue.component(TstCheckbox.name, TstCheckbox)
 Vue.component(TstSelect.name, TstSelect)
+Vue.component(TstFontsize.name, TstFontsize)
 
 export {
     TstApexPanel,
