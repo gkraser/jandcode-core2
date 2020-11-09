a4ac6226b3d0c1239c702e66179d6ecd698bc009
gkraser <gkraser@gmail.com>
2020-11-09 22:49:08 +0600

    apx

---
+++ p-apx/jandcode-core-apx-webapp/src/jandcode/core/apx/webapp/_tst/pages/IconList1.test.vue
@@ -0,0 +1,36 @@
+<template>
+    <tst-apx-panel class="iconlist1-test-b83df47e">
+        <template #tools>
+        </template>
+
+        <tst-icon-list/>
+
+    </tst-apx-panel>
+</template>
+
+<script>
+export default {
+    extends: Vue.component('tst-apx-page'),
+    components: {},
+    created() {
+        this.cfgStore.applyDefault({})
+    },
+    data() {
+        return {}
+    },
+    methods: {
+        applyCfg() {
+            let cfg = this.cfg
+        },
+    }
+}
+</script>
+
+<style lang="less">
+
+.iconlist1-test-b83df47e {
+
+
+}
+
+</style>
