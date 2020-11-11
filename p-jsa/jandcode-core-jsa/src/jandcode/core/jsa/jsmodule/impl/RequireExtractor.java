cadbc3a4ed0fd477de30655aaa49f48b988f326c
gkraser <gkraser@gmail.com>
2020-11-11 09:05:36 +0600

    jsa: fix extractRequire for webroot/jslib

--- p-jsa/jandcode-core-jsa/src/jandcode/core/jsa/jsmodule/impl/RequireExtractor.java
+++ p-jsa/jandcode-core-jsa/src/jandcode/core/jsa/jsmodule/impl/RequireExtractor.java
@@ -11,6 +11,7 @@ public class RequireExtractor {
     public static Pattern P_REMOVE_COMMENT_ML = Pattern.compile("\\/\\*.*?\\*\\/", Pattern.DOTALL | Pattern.MULTILINE);
     public static Pattern P_REMOVE_COMMENT_1L = Pattern.compile("//.*");
     public static Pattern P_STRING_CONST = Pattern.compile("(['|\"])(.*?\\/\\*.*?)\\1");
+    public static String REQUIRE_MARK = "require(";
 
     /**
      * Извлекает require из исходника на js
@@ -23,6 +24,12 @@ public class RequireExtractor {
 
         if (UtString.empty(source)) {
             return res;
+        }
+
+        // по быстрому проверяем, есть ли require в принципе
+        int rPos = source.indexOf(REQUIRE_MARK);
+        if (rPos == -1) {
+            return res;  // нет require
         }
 
         // зменяем * в константах на &, что бы они в коментариях не учитывались
