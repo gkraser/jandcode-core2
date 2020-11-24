4107ba624ee4697b5bc32ff05189b331fef35a39
gkraser <gkraser@gmail.com>
2020-11-24 20:27:43 +0600

    apx: test: createDao

--- p-apx/jandcode-core-apx/src/jandcode/core/apx/test/ApxTestSvc.java
+++ p-apx/jandcode-core-apx/src/jandcode/core/apx/test/ApxTestSvc.java
@@ -1,11 +1,37 @@
 package jandcode.core.apx.test;
 
-import jandcode.commons.test.*;
+import jandcode.commons.*;
+import jandcode.core.dao.*;
+import jandcode.core.test.*;
 
 /**
  * Расширение для тестов: поддержка apx
  */
-public class ApxTestSvc extends BaseTestSvc {
+public class ApxTestSvc extends BaseAppTestSvc {
 
+    /**
+     * Создать экземпляр dao.
+     *
+     * @param cls            для какого класса
+     * @param daoInvokerName какой daoInvoker использовать.
+     *                       Если не указано - 'default'. Можно например указать модельный 'model:default'
+     */
+    public <A> A createDao(Class<A> cls, String daoInvokerName) throws Exception {
+        if (UtString.empty(daoInvokerName)) {
+            daoInvokerName = "default";
+        }
+        DaoService svc = getApp().bean(DaoService.class);
+        DaoInvoker di = svc.getDaoInvoker(daoInvokerName);
+        return di.createDao(cls);
+    }
+
+    /**
+     * Создать экземпляр dao для daoInvoker=default
+     *
+     * @param cls для какого класса
+     */
+    public <A> A createDao(Class<A> cls) throws Exception {
+        return createDao(cls, null);
+    }
 
 }
