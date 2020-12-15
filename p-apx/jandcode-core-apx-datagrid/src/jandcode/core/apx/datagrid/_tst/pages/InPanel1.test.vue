c3ff1f48df13e078ea15631aeff9fe1cafa13b04
gkraser <gkraser@gmail.com>
2020-12-15 09:40:55 +0600

    apx: fix grid styles

--- p-apx/jandcode-core-apx-datagrid/src/jandcode/core/apx/datagrid/_tst/pages/InPanel1.test.vue
+++ p-apx/jandcode-core-apx-datagrid/src/jandcode/core/apx/datagrid/_tst/pages/InPanel1.test.vue
@@ -1,11 +1,12 @@
 <template>
     <tst-apx-panel class="inpanel1-test-c135e6b6">
         <template #tools>
+            <tst-btn label="dialog1" @click="dialog1"/>
         </template>
 
         <div class="row q-gutter-x-md q-mb-md" style="height:250px">
 
-            <jc-panel class="col" title="Это панель 2" body-fit>
+            <jc-panel class="col" title="Это панель 2" body-fit no-padding>
                 <template #toolbar>
                     <jc-action icon="more-v"/>
                     <jc-action icon="bus" label="Привет"/>
@@ -18,14 +19,18 @@
                 </template>
             </jc-panel>
 
-            <jc-panel class="col" body-fit>
-                <jc-datagrid :options="opt2" style1="width:500px"/>
-                <template #footer>
-                    Это footer
+            <jc-panel class="col" title="Грида" body-fit no-padding>
+                <template #toolbar>
+                    <jc-action icon="more-v"/>
+                    <jc-action icon="bus" label="Привет"/>
                 </template>
+                <jc-datagrid :options="opt2" style1="width:500px"/>
+<!--                <template #footer>-->
+<!--                    Это footer-->
+<!--                </template>-->
             </jc-panel>
 
-            <jc-panel class="col-4" body-fit>
+            <jc-panel class="col-4" body-fit no-padding>
                 <jc-datagrid :options="opt2" style1="width:500px"/>
             </jc-panel>
 
@@ -35,8 +40,9 @@
 </template>
 
 <script>
-import {tst} from '../vendor'
+import {apx, tst} from '../vendor'
 import * as grids from './grids'
+import Dialog1 from './Dialog1'
 
 export default {
     mixins: [tst.mixins.cfgStore],
@@ -54,6 +60,11 @@ export default {
         applyCfg() {
             let cfg = this.cfg
         },
+        dialog1() {
+            apx.showDialog({
+                frame: Dialog1
+            })
+        }
     }
 }
 </script>
