28b9424180ea3db87aaf8ceb338021a266f27adf
gkraser <gkraser@gmail.com>
2021-01-12 10:02:40 +0600

    apx: font roboto изменил название пакета

--- p-apx/jandcode-core-apx-webui/src/jandcode/core/apx/webui/css/fonts/font-roboto.js
+++ p-apx/jandcode-core-apx-webui/src/jandcode/core/apx/webui/css/fonts/font-roboto.js
@@ -1,12 +1,12 @@
-import f100 from "fontsource-roboto/100.css";
-import f300 from "fontsource-roboto/300.css";
-import f400 from "fontsource-roboto/400.css";
-import f500 from "fontsource-roboto/500.css";
-import f700 from "fontsource-roboto/700.css";
-import f900 from "fontsource-roboto/900.css";
+import f100 from "@fontsource/roboto/100.css";
+import f300 from "@fontsource/roboto/300.css";
+import f400 from "@fontsource/roboto/400.css";
+import f500 from "@fontsource/roboto/500.css";
+import f700 from "@fontsource/roboto/700.css";
+import f900 from "@fontsource/roboto/900.css";
 
-import f400c from "fontsource-roboto-condensed/400.css";
-import f300c from "fontsource-roboto-condensed/300.css";
+import f400c from "@fontsource/roboto-condensed/400.css";
+import f300c from "@fontsource/roboto-condensed/300.css";
 
 export default [
     f100, f300, f400, f500, f700, f900,
