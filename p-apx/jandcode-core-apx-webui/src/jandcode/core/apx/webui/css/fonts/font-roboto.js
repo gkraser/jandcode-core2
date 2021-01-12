63d53463073790a17f5d7e6ee29b2492a40b5f09
gkraser <gkraser@gmail.com>
2021-01-12 17:10:26 +0600

    jsa: font roboto-mono

--- p-apx/jandcode-core-apx-webui/src/jandcode/core/apx/webui/css/fonts/font-roboto.js
+++ p-apx/jandcode-core-apx-webui/src/jandcode/core/apx/webui/css/fonts/font-roboto.js
@@ -8,8 +8,12 @@ import f900 from "@fontsource/roboto/900.css";
 import f400c from "@fontsource/roboto-condensed/400.css";
 import f300c from "@fontsource/roboto-condensed/300.css";
 
+import f400m from "@fontsource/roboto-mono/400.css";
+import f700m from "@fontsource/roboto-mono/700.css";
+
 export default [
     f100, f300, f400, f500, f700, f900,
     f400c, f300c,
+    f400m, f700m,
 ]
 
