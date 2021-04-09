60c93808bc923bb52dab594788ff74fee3e745bd
gkraser <gkraser@gmail.com>
2021-04-09 18:01:02 +0600

    jc: JcEnvBuilder: метод append

--- p-jc/jandcode-jc/src/jandcode/jc/JcEnvBuilder.groovy
+++ p-jc/jandcode-jc/src/jandcode/jc/JcEnvBuilder.groovy
@@ -4,12 +4,14 @@ import jandcode.commons.*
 import jandcode.jc.std.*
 
 /**
- * Построитель файла для настройки среды исполнения jc: _jc/jc-env.bat.
- * Делаем include(JcEnvBuilder) в обработчике события
- * {@link PrepareProject.Event_Prepare}. Свойству text добавляем строки,
- * которые должны стать частью файла jc-env.bat.
- * Учитываем среду исполнения (windows, linux) {@link jandcode.commons.UtFile#isWindows()}
+ * Построитель файла для настройки среды исполнения jc: _jc/jc-env.bat
+ * (_jc/jc-env.sh для linux).
  *
+ * Делаем include(JcEnvBuilder) в обработчике события
+ * {@link PrepareProject.Event_Prepare}. Методом append() добавляем строки,
+ * которые должны стать частью файла jc-env.bat/jc-env.sh
+ * .
+ * Учитываем среду исполнения (windows, linux) {@link jandcode.commons.UtFile#isWindows()}
  */
 class JcEnvBuilder extends ProjectScript implements IPrepareProject {
 
@@ -34,6 +36,16 @@ class JcEnvBuilder extends ProjectScript implements IPrepareProject {
 
         ant.mkdir(dir: wd(JcConsts.JC_METADATA_DIR))
         UtFile.saveString(text1, new File(wd(JcConsts.JC_METADATA_DIR + "/${fnEnv}")))
+    }
+
+    /**
+     * Добавить текст в jc-env.bat/jc-env.sh
+     */
+    void append(String s) {
+        if (this.text.length() > 0) {
+            this.text.append("\n")
+        }
+        this.text.append(s)
     }
 
 }
