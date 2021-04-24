aa89582e58246a7336122fae75872cdb8f99fdd4
gkraser <gkraser@gmail.com>
2021-04-24 21:14:14 +0600

    web: централизованная сборка ресурсов для web-resource-webroot.jar

--- p-web/jandcode-core-nodejs-jc/src/jandcode/core/nodejs/jc/NodeJsRootProject.groovy
+++ p-web/jandcode-core-nodejs-jc/src/jandcode/core/nodejs/jc/NodeJsRootProject.groovy
@@ -1,7 +1,7 @@
 package jandcode.core.nodejs.jc
 
 import jandcode.commons.*
-import jandcode.core.jc.*
+import jandcode.core.web.jc.*
 import jandcode.jc.*
 import jandcode.jc.std.*
 
@@ -20,9 +20,6 @@ class NodeJsRootProject extends ProjectScript {
         include(BuildProject)
         onEvent(BuildProject.Event_Build, this.&buildHandler)
 
-        // product
-        onEvent(AppProductBuilder.Event_Exec, this.&productHandler)
-
         cm.add("nodejs-build", "Выполнить сборку клиенского кода (npm run build)", this.&cmNodejsBuild,
                 cm.opt('prod', 'Запустить в режиме production')
         )
@@ -30,13 +27,14 @@ class NodeJsRootProject extends ProjectScript {
                 cm.opt('prod', 'Запустить в режиме production')
         )
 
+        //
+        afterLoad {
+            // откуда забираем скомпиленное
+            String srcPath = wd.dir(mainModule).join(webrootDir)
+            // включаем его в jar для продукта
+            include(WebRootProject).addResourceWebroot(srcPath)
+        }
     }
-
-    /**
-     * Корневой каталог ресурсов для скомпилированных файлов nodejs.
-     * По соглашению с модулем jandcode-core-web
-     */
-    String resourceRoot = "resource-webroot"
 
     /**
      * Корневой каталог внутри mainModule, куда осуществляется генерация файлов.
@@ -73,13 +71,6 @@ class NodeJsRootProject extends ProjectScript {
 
     //////
 
-    /**
-     * Полное имя jar-файла со скомпиленными клиенскими файлами.
-     */
-    String getFileNodejsWebrootJar() {
-        return wd("temp/lib/nodejs-${resourceRoot}.jar")
-    }
-
     void cmNodejsWatch(CmArgs args) {
         boolean isProd = args.containsKey('prod')
 
@@ -103,31 +94,10 @@ class NodeJsRootProject extends ProjectScript {
         }
         log "npm run build in ${mainModule}"
         ut.runcmd(cmd: "jc @ ${env} npm run build", dir: mainModule)
-
-        if (ctx.env.prod) {
-            log.info("build jar: ${getFileNodejsWebrootJar()}")
-
-            // откуда забираем скомпиленное
-            String srcPath = wd.dir(mainModule).join(webrootDir)
-
-            // собираем jar
-            ant.jar(destfile: getFileNodejsWebrootJar()) {
-                zipfileset(dir: srcPath, prefix: "${resourceRoot}") {
-                    include(name: '**/*')
-                }
-            }
-        }
     }
 
     void buildHandler() {
         cm.exec("nodejs-build")
-    }
-
-    void productHandler(AppProductBuilder.Event_Exec e) {
-        AppProductBuilder builder = e.builder
-
-        // nodejs-resouurce-webroot.jar
-        ant.copy(file: getFileNodejsWebrootJar(), todir: "${builder.destDir}/lib")
     }
 
 }
--- p-web/jandcode-core-web-jc/src/jandcode/core/web/jc/WebRootProject.groovy
+++ p-web/jandcode-core-web-jc/src/jandcode/core/web/jc/WebRootProject.groovy
@@ -1,6 +1,7 @@
 package jandcode.core.web.jc
 
-
+import jandcode.commons.*
+import jandcode.commons.error.*
 import jandcode.core.jc.*
 import jandcode.jc.*
 import jandcode.jc.std.*
@@ -20,8 +21,55 @@ class WebRootProject extends ProjectScript {
         onEvent(AppProductBuilder.Event_Exec, this.&onAppProductBuild)
     }
 
+    protected List<String> _resourceWebrootPaths = []
+
+    /**
+     * Добавить путь, который будет запакован в web-resource-webroot.jar при
+     * сборке дистрибутива.
+     */
+    void addResourceWebroot(String path) {
+        _resourceWebrootPaths.add(path)
+    }
+
+    /**
+     * Собрать web-resource-webroot.jar и вернуть путь до собранного файла.
+     * Возвращает null, если собирать нечего.
+     */
+    String buildResourceWebrootJar() {
+        if (_resourceWebrootPaths.size() == 0) {
+            return null
+        }
+
+        String resourceRoot = "resource-webroot"
+        String destFile = wd("temp/lib/web-resource-webroot.jar")
+        log.info("build jar: ${destFile}")
+
+        // собираем jar
+        ant.jar(destfile: destFile) {
+            for (srcPath in this._resourceWebrootPaths) {
+                if (!UtFile.exists(srcPath)) {
+                    throw new XError("Not found resourceWebrootPath: ${srcPath}")
+                }
+                zipfileset(dir: srcPath, prefix: "${resourceRoot}") {
+                    include(name: '**/*')
+                }
+            }
+        }
+
+        return destFile
+    }
+
     void onAppProductBuild(AppProductBuilder.Event_Exec ev) {
+        AppProductBuilder builder = ev.builder
+
         include(GenWebXml).genWebXml("${ev.builder.destDir}/web.xml")
+
+        // web-resouurce-webroot.jar
+        String rwr = buildResourceWebrootJar()
+        if (rwr != null) {
+            //
+            ant.copy(file: rwr, todir: "${builder.destDir}/lib")
+        }
     }
 
 }
